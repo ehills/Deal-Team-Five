910b21ab2bc931433173b3922a6fd0c0aae5eab6
Edward Hills <ehills666@gmail.com>
2011-08-04 17:09:40 +1200

    re wrote css and html

--- private_application/views/coming_soon.php
+++ private_application/views/coming_soon.php
@@ -1,30 +1,23 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>DealTeam5.co.nz - Ask for a deal. We also stock Electronics, Computers, Games, Daily Deals, Music, Movies, Toys &amp; Collectibles and much more...</title>
-<link href="css/style.css" rel="stylesheet" type="text/css" />
-</head>
-
-<body>
-
-<div id="mainContainer">
-  
-  <div id="content">
-  	<div class="mainImageHolder"><img src="images/main-image.png" alt="feature image"/>
-    <!--end .mainImageHolder--></div>
-    <!-- end .content --></div>
- <div id="footerContainer">
- <form id="form" action="" method="post" name="form" >
-  	<div class="fieldHolder"><input type="text" name="email" class="textInput" id="emailInput" /><!--end .fieldHolder--></div>
-    <div class="fieldHolder"><input type="image" class="button" src="images/subscribe-button.png"  /><!--end .fieldHolder--></div>
-    <div id="socialButtonHolder">
-    <div class="fieldHolder"><input type="image" class="facebook" src="images/facebook-button.png"  /><!--end .fieldHolder--></div>
-    <div class="fieldHolder"><input type="image" class="twitter" src="images/twitter-button.png"  /><!--end .fieldHolder--></div>
-    <!--end #socialButtonHolder--></div>
-	</form>
-    <!-- end .footerContainer--></div>
-  <!-- end .mainContainer --></div>
-  
-</body>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>DealTeam5.co.nz - Ask for a deal. We also stock Electronics, Computers, Games, Daily Deals, Music, Movies, Toys &amp; Collectibles and much more...</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<div id="mainContainer">
+			
+			<form id="form" action="" method="post" name="form" >
+				<input type="text" name="email" id="email" />
+				<input type="submit" class="button" value="Submit" />
+			</form>
+			
+			<div id="socialButtonHolder">
+				<a href="#"><img src="images/facebook-button.png" alt="Like us on Facebook" width="34" height="33"/></a>
+				<a href="#"><img src="images/twitter-button.png" alt="Follow us on Twitter" width="34" height="33"/></a>
+			</div>
+			
+		</div>
+	</body>
 </html>
