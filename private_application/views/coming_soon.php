40f55606a9923d185ebd9893c5e4c5b0fc2625da
Edward Hills <ehills666@gmail.com>
2011-08-04 17:27:15 +1200

    fixed stupid ie border outline thing

--- private_application/views/coming_soon.php
+++ private_application/views/coming_soon.php
@@ -1,5 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html
+PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title>DealTeam5.co.nz - Ask for a deal. We also stock Electronics, Computers, Games, Daily Deals, Music, Movies, Toys &amp; Collectibles and much more...</title>
